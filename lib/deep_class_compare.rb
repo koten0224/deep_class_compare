c533494b7b2d6bea787ede6cce62b2d1b2865b57
Koten <koten0224@gmail.com>
2021-12-23 00:22:51 +0800

    init commit

---
+++ lib/deep_class_compare.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "deep_class_compare/version"
+
+module DeepClassCompare
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/deep_class_compare/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DeepClassCompare
+  VERSION = "0.1.0"
+end
