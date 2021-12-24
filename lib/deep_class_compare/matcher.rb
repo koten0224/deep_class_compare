850b9c59e2eceb2e8b16a783f73e013f55e21e8b
Koten <koten0224@gmail.com>
2021-12-25 01:55:17 +0800

    feat(base matcher): initial base matcher

---
+++ lib/deep_class_compare/matcher.rb
@@ -0,0 +1,18 @@
+
+module DeepClassCompare
+  class Matcher
+    def initialize(base)
+      @base = base
+      raise_class_required_error! unless @base.is_a?(Class)
+    end
+
+    def match?(value)
+      value.is_a?(@base)
+    end
+
+    private
+    def raise_class_required_error!
+      raise TypeError.new("class or module required")
+    end
+  end
+end
