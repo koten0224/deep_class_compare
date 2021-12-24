78b599f2ec50068d1567e87304c3feca78df91d4
Koten <koten0224@gmail.com>
2021-12-25 06:15:30 +0800

    use require instead of require_relative

--- lib/deep_class_compare.rb
+++ lib/deep_class_compare.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "deep_class_compare/version"
-require_relative "deep_class_compare/matcher"
-require_relative "deep_class_compare/array_matcher"
-require_relative "deep_class_compare/hash_matcher"
+require "./lib/deep_class_compare/version"
+require "./lib/deep_class_compare/matcher"
+require "./lib/deep_class_compare/array_matcher"
+require "./lib/deep_class_compare/hash_matcher"
 module DeepClassCompare
   class Error < StandardError; end
   class TypeError < Error; end
