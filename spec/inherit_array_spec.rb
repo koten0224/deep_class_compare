a1afa6d9ba4d259f8baae2eeac04715526a1b410
Koten <koten0224@gmail.com>
2022-01-01 03:00:58 +0800

    feat(inherit array): extend rspec

--- spec/inherit_array_spec.rb
+++ spec/inherit_array_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe InheritArray do
       subject { described_class.of(Object) }
       it { expect { subject }.not_to raise_error }
       it { expect { subject.of(Object) }.to raise_error }
+      it { expect { described_class.of(described_class).of(described_class) }.not_to raise_error }
     end
 
     context "#like_a?" do
@@ -27,6 +28,34 @@ RSpec.describe InheritArray do
         expect(params).not_to be_like_a InheritArray.of Integer
         expect([]).not_to be_like_a InheritArray
         expect(['string']).not_to be_like_a InheritArray.of String
+      end
+
+      context "compare with nested inherit array" do
+        let(:params) do
+          array = InheritArray.new
+          array << InheritArray.new
+          array.first << 'string'
+          array
+        end
+
+        it 'return true' do
+          expect(params).to be_like_a InheritArray.of InheritArray
+          expect(params).to be_like_a Array.of InheritArray
+          expect(params).to be_like_a Array.of Array
+          expect(params).to be_like_a InheritArray.of(InheritArray).of(String)
+          expect(params).to be_like_a InheritArray.of InheritArray.of String
+          expect(params).to be_like_a Array.of(Array).of(String)
+          expect(params).to be_like_a Array.of Array.of String
+        end
+
+        it 'return false' do
+          expect(params).not_to be_like_a InheritArray.of String
+          expect(params).not_to be_like_a Array.of String
+          expect(params).not_to be_like_a InheritArray.of(InheritArray).of(Integer)
+          expect(params).not_to be_like_a InheritArray.of InheritArray.of Integer
+          expect(params).not_to be_like_a Array.of(Array).of(Integer)
+          expect(params).not_to be_like_a Array.of Array.of Integer
+        end
       end
     end
   end
